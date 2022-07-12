80bed1c868123ee9819ebfaaff24d91f5b9a22ab
Dan Harsanyi <dan@danharsanyi.com>
2022-07-12 22:21:34 +1000

    WIP Job board

---
+++ graphql-by-example/job-board/server/resolvers.js
@@ -0,0 +1,12 @@
+import { Job } from './db.js';
+import { Company } from './db.js';
+
+export const resolvers = {
+  Query: {
+    jobs: () => Job.findAll(),
+  },
+
+  Job: {
+    company: job => Company.findById(job.companyId)
+  }
+}
